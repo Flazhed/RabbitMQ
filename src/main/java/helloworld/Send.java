ae37477c8ddf4c5b3aea532835326a117221c316
Soren <soren@soren.soren>
2016-09-20 11:14:19 +0200

    pls

---
+++ src/main/java/helloworld/Send.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * @author Soren
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "helloper";
+
+    public static void main(String[] argv)
+            throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("datdb.cphbusiness.dk");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Per er en guttermand";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+}
